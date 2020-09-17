bafcddf9af17def004fff70d3c008d28912239fe
Alex <revolalex@gmail.com>
2020-09-17 19:02:09 +0200

    work in progress

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,15 +1,15 @@
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-const appRouter = async function(app, connection) {
-  app.post("/sign-up", function(req, res) {
-    var name = req.body.name;
-    var email = req.body.email;
-    var passTemp = req.body.password;
 
+const appRouter = async function(app, connection) {
+  // add an user ==> /sign-up
+  app.post("/sign-up", function(req, res) {
+    let name = req.body.name;
+    let email = req.body.email;
+    let passTemp = req.body.password;
     // hash the password
     let pass = bcrypt.hashSync(passTemp, saltRounds);
-
     const userObject = {
       name,
       email,
@@ -25,6 +25,7 @@ const appRouter = async function(app, connection) {
     res.send(name);
   });
 
+  // Check if an user is register ==> /sign-in
   app.post("/sign-in", function(req, res) {
     let email = req.body.email;
     let pass = req.body.password;
@@ -34,15 +35,85 @@ const appRouter = async function(app, connection) {
     connection.query(mailUser, [email], function(err, results, fields) {
       if (err) throw err;
       console.log("mail ==>", results);
-      hash = results[0].pass;
+      // handle email error
+      if (results.length < 1) {
+        res.send("Sorry, email incorrect");
+      } else {
+        hash = results[0].pass;
+        // handle password error
+        bcrypt.compare(pass, hash, function(err, result) {
+          if (result == true) {
+            res.send("you are authenticated");
+          } else {
+            res.send("Sorry, password incorrect");
+          }
+        });
+      }
+    });
+  });
 
-      bcrypt.compare(pass, hash, function(err, result) {
-        if (result == true) {
-          res.send("you are authenticated");
-        } else {
-          res.send("Sorry, password incorrect");
-        }
-      });
+  // get all database ==> /all
+  app.get("/all", function(req, res) {
+    let getAll = "SELECT * FROM authentification.users";
+    connection.query(getAll, function(err, results) {
+      if (err) throw err;
+      res.send(results);
+    });
+  });
+
+  // get all emails ==> /emails
+  app.get("/emails", function(req, res) {
+    let getAllEmail = "SELECT email FROM authentification.users";
+    connection.query(getAllEmail, function(err, results) {
+      if (err) throw err;
+      res.send(results);
+    });
+  });
+
+  // get all names ==> /names
+  app.get("/names", function(req, res) {
+    let getAllNames = "SELECT name FROM authentification.users";
+    connection.query(getAllNames, function(err, results) {
+      if (err) throw err;
+      res.send(results);
+    });
+  });
+
+  // delete user with this email ==> /users/:email
+  app.delete("/users/:email", function(req, res) {
+    let email = req.params.email;
+    let usersMailToDelete =
+      "DELETE FROM authentification.users where email = ?";
+    connection.query(usersMailToDelete, [email], function(err, results) {
+      if (err) throw err;
+      // handle unknown user
+      if ((results.affectedRows > 0)) {
+        console.log(results.affectedRows);
+        res.send("Users removed");
+      }else {
+        res.send("Unknown users");
+      }
+    });
+  });
+
+  // create a database ==> /createDB
+  app.post("/createDB", function(req, res) {
+    let dbName = "'" + req.body.dbName +  "'";
+    let createDatabase = "CREATE DATABASE ?";
+    connection.query(createDatabase, [dbName], function(err, results) {
+      if (err) throw err;
+      res.send(results);
+    });
+  });
+
+  // delete database ==> /db/:dbName
+  app.delete("/db/:dbName", function(req, res) {
+    let dbName = req.params.dbName;
+    console.log(dbName);
+    let dbToDelete = "DROP DATABASE ?";
+    connection.query(dbToDelete, [dbName], function(err, results) {
+      if (err) throw err;
+      res.send(results);
     });
   });
 };
