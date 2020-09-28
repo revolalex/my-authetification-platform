0ad3e5b3d861e374b127db014a1708eebd470028
Alex <revolalex@gmail.com>
2020-09-28 12:03:04 +0200

    signin vuelidate done

--- front-vue/src/components/SignInForm.vue
+++ front-vue/src/components/SignInForm.vue
@@ -2,19 +2,35 @@
   <div>
     <b-form @submit="onSubmit" v-if="show">
       <!-- email -->
-      <b-form-group id="input-group-1" label="Email address:" label-for="input-1">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        invalid-feedback="Valid email is required"
+      >
         <b-form-input
+          :state="validateState('email')"
           id="input-1"
-          v-model="form.email"
+          v-model="$v.form.email.$model"
           type="email"
-          required
           placeholder="Enter email"
         ></b-form-input>
       </b-form-group>
 
       <!-- password -->
-      <b-form-group id="input-group-2" label="Your Password:" label-for="input-2">
-        <b-form-input id="input-2" v-model="form.password" required placeholder="Enter password"></b-form-input>
+      <b-form-group
+        id="input-group-2"
+        label="Your Password:"
+        label-for="input-2"
+        invalid-feedback="Password is required, minimun 8 characters"
+      >
+        <b-form-input
+        :state="validateState('password')"
+        v-model="$v.form.password.$model"
+          id="input-2"
+          required
+          placeholder="Enter password"
+        ></b-form-input>
       </b-form-group>
       <!-- button -->
       <b-button type="submit" variant="success">Sign In</b-button>
@@ -28,7 +44,10 @@
 
 <script>
 import axios from "axios";
+import { validationMixin } from "vuelidate";
+import { required, minLength, email } from "vuelidate/lib/validators";
 export default {
+  mixins: [validationMixin],
   name: "SignInForm",
   data() {
     return {
@@ -39,8 +58,24 @@ export default {
       show: true,
     };
   },
+  validations: {
+    form: {
+      email: {
+        required,
+        email: email,
+      },
+      password: {
+        required,
+        minLength: minLength(8),
+      },
+    },
+  },
   // mounted: function (){console.log(this.$store.state.token)},
   methods: {
+    validateState(name) {
+      const { $dirty, $error } = this.$v.form[name];
+      return $dirty ? !$error : null;
+    },
     onSubmit(evt) {
       let that = this;
       evt.preventDefault();
@@ -52,7 +87,6 @@ export default {
             console.log("success");
             that.$store.dispatch("ADD_TOKEN", response.data.token);
             that.$router.push("/Dashboard");
-            
           } else {
             alert("Error password or email incorrect");
           }
